fe730b891356711d47875d73cc0824627dd78ab0
sam-fsd <kabirusam037@gmail.com>
2024-03-04 12:49:33 +0300

    useEffect to fetch data from json server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Persons } from './components/Persons';
 import { Filter } from './components/Filter';
 import { Form } from './components/Form';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchValue, setSearchValue] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addInfo = (event) => {
     event.preventDefault();
@@ -47,7 +49,7 @@ const App = () => {
     const filteredPersons = persons.filter((person) =>
       person.name.toLowerCase().includes(val)
     );
-    setFilteredPersons(filteredPersons); // Update entire persons state with filtered data
+    setFilteredPersons(filteredPersons); // Update entire filteredPersons state with filtered data
   };
 
   return (
