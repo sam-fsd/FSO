9ef545ced4ce639fb1728391b0c514af5dc1bc70
sam-fsd <kabirusam037@gmail.com>
2024-07-29 11:38:31 +0300

    React Query to handle server-side state. EXS[6.20 - 6.22]

---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/anecdotes';
+
+export const getAll = () => axios.get(baseUrl).then((res) => res.data);
+
+export const createNew = (newAnecdote) =>
+  axios.post(baseUrl, newAnecdote).then((res) => res.data);
+
+export const castVote = (anecdote) =>
+  axios.put(`${baseUrl}/${anecdote.id}`, anecdote);
